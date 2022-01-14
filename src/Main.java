6c9bfc5968764face9c95d919484659834e3ca86
kuromame <moeruensyo0546@gmail.com>
2022-01-14 13:17:58 +0900

    500*500のフレームを表示

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
+import java.awt.Color;
 
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
+        JFrame frame = new JFrame("paint");
+        frame.setSize(500, 500);//windowのサイズ
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//×を押した時の動作設定
+        frame.setLayout(null);
+        frame.setResizable(false);
+
+        frame.setVisible(true);//表示
     }
 }
