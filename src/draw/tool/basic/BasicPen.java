84bd2a78efaa3af36340a7287c9b7045b9b1a47a
kuromame <moeruensyo0546@gmail.com>
2022-01-14 16:57:07 +0900

    Basicペンの書き味を滑らかにした！！！！

--- src/draw/tool/basic/BasicPen.java
+++ src/draw/tool/basic/BasicPen.java
@@ -7,24 +7,56 @@ import java.awt.event.MouseEvent;
 
 public class BasicPen implements DrawTool {
 
-    private static final int OVAL_SIZE = 5;
+    private int x;
+    private int y;
 
-    public BasicPen init() {
+
+
+    public BasicPen(){
+        initializePosition();
+    }
+
+    public DrawTool init(){
+        initializePosition();
         return this;
     }
+
+    private static final int initialPosition = -1;
+
+    private boolean isFirst(){
+        return x == initialPosition || y == initialPosition;
+    }
+
+    private void initializePosition(){
+        setPosition(initialPosition, initialPosition);
+    }
+
+    private void setPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
 
     public void mouseClicked(MouseEvent e, Graphics graphics) {}
 
     public void mousePressed(MouseEvent e, Graphics graphics) {}
 
-    public void mouseReleased(MouseEvent e, Graphics graphics) {}
+    public void mouseReleased(MouseEvent e, Graphics graphics) {
+        initializePosition();
+    }
 
     public void mouseEntered(MouseEvent e, Graphics graphics) {}
 
     public void mouseExited(MouseEvent e, Graphics graphics) {}
 
     public void mouseDragged(MouseEvent e, Graphics graphics) {
-        graphics.fillOval(e.getX(), e.getY(),OVAL_SIZE,OVAL_SIZE);
+        if(isFirst()){
+            setPosition(e.getX(),e.getY());
+            return;
+        }
+        graphics.drawLine(x,y, e.getX(), e.getY());
+        initializePosition();
+        setPosition(e.getX(), e.getY());
     }
 
     public void mouseMoved(MouseEvent e, Graphics graphics) {}
