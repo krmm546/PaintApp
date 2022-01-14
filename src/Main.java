0f4f4470fb1f21d61c9d986541adad777383ea59
kuromame <moeruensyo0546@gmail.com>
2022-01-14 13:45:07 +0900

    メニューパネルとペイントパネルの追加

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -6,11 +6,26 @@ import javax.swing.border.LineBorder;
 public class Main {
     public static void main(String[] args) {
 
+        int frameLength = 500;
+
         JFrame frame = new JFrame("paint");
-        frame.setSize(500, 500);//windowのサイズ
+        frame.setSize(frameLength, frameLength);//windowのサイズ
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//×を押した時の動作設定
         frame.setLayout(null);
         frame.setResizable(false);
+
+        int menuHeight = (int)(frameLength * 0.12);
+
+
+        JPanel menuPanel = new JPanel();
+        menuPanel.setBounds(0, 0, frameLength, menuHeight);
+        menuPanel.setBackground(Color.gray);
+        frame.add(menuPanel);
+
+        JPanel paintPanel = new JPanel();
+        paintPanel.setBounds(0,menuHeight, frameLength,frameLength - menuHeight);
+        frame.add(paintPanel);
+
 
         frame.setVisible(true);//表示
     }
