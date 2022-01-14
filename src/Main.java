f1acad97a776da638d17c4a98c5839eba0ee9900
kuromame <moeruensyo0546@gmail.com>
2022-01-14 14:02:46 +0900

    コンポーネントの名前をコメントにつけた

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
         int frameLength = 500;
 
+        // フレーム
         JFrame frame = new JFrame("paint");
         frame.setSize(frameLength, frameLength);//windowのサイズ
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//×を押した時の動作設定
@@ -17,18 +18,21 @@ public class Main {
 
         int menuHeight = (int)(frameLength * 0.12);
 
-
+        // メニュー
         JPanel menuPanel = new JPanel();
         menuPanel.setBounds(0, 0, frameLength, menuHeight);
         menuPanel.setBackground(Color.gray);
         frame.add(menuPanel);
 
+        // 描画パレット
         JPanel paintPanel = new JPanel();
         paintPanel.setBounds(0,menuHeight, frameLength,frameLength - menuHeight);
 
+        // 描画リスナー
         PaintPanelListener paintPanelListener = new PaintPanelListener(paintPanel);
         paintPanel.addMouseListener(paintPanelListener);
         paintPanel.addMouseMotionListener(paintPanelListener);
+
         frame.add(paintPanel);
 
 
